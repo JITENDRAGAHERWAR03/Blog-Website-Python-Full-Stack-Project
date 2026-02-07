93654dfb360cf1e24852ac8c0ad48290dc7412ad
Jitendra k Gaherwar <jitendragahewar03@gmail.com>
2026-02-07 21:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from models import db, BlogPost
 
@@ -31,5 +32,8 @@ def create():
         return redirect(url_for('index'))
     return render_template('create.html')
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
